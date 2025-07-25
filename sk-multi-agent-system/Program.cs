cb013d228261c32de3e96696e115af2f78c26766
Kamadi Liyanage <kamadi.liyanage@ultimo.com>
2025-07-25 20:34:57 +0530

    initial commit

--- sk-multi-agent-system/Program.cs
+++ sk-multi-agent-system/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+string API_KEY = "";
+string MODEL_ID = "gpt-4.1";
+
+// Create a Kernel
+var builder = Kernel.CreateBuilder();
+builder.AddOpenAIChatCompletion(MODEL_ID, API_KEY);
+var kernel = builder.Build();
+
+// Get the chat service
+var chatService = kernel.GetRequiredService<IChatCompletionService>();
+
+// Create a chat history
+ChatHistory chatMessages = new ChatHistory();
+chatMessages.AddUserMessage("What's the capital of France?");
+
+// Send the chat to the LLM and get a response
+var reply = await chatService.GetChatMessageContentAsync(chatMessages);
+
+// Print the result
+Console.WriteLine($"Assistant: {reply.Content}");
